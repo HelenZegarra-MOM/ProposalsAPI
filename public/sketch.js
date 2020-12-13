e2c9fb6501cc5a03bed56981ce6aef2625c03ea8
HelenSteph <61945655+HelenSteph@users.noreply.github.com>
2020-12-13 11:54:44 -0500

    V.2.0
    Updated version of the  front-end

--- public/sketch.js
+++ public/sketch.js
@@ -1,21 +1,28 @@
 function setup() {
   noCanvas();
-  testPost();
-}
+let keywords, proposal, candidate, politicalparty;
 
-async function testPost() {
-  let proposal = {
-    text: 'hello',
-    politicalp: 'a',
-    year: '1900',
-    candidate: 'test',
-  };
-  const data = {
+const button = document.getElementById('submit');
+button.addEventListener('click', async event => {
+const keywords = document.getElementById('keywords').value;
+const proposal = document.getElementById('proposal').value;
+const candidate = document.getElementById('candidate').value;
+const politicalparty= document.getElementById('politicalparty').value;
+
+const data = { keywords, proposal, candidate, politicalparty };
+
+const options = {
     method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify(proposal),
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data)
   };
-  const response = await fetch('proposal', data);
+  const response = await fetch('/proposals', options);
   const json = await response.json();
   console.log(json);
+});
+
+
+
 }
