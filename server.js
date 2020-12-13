4e9f8e86cb54ce23d81077eb496497cff0614783
HelenSteph <61945655+HelenSteph@users.noreply.github.com>
2020-12-13 11:52:51 -0500

    V.2.0
    New version of the API

--- server.js
+++ server.js
@@ -1,188 +1,50 @@
 console.log('Server starting ...');
 const express = require('express');
-const bodyParser = require('body-parser');
+const Datastore = require('nedb');
+
 const app = express();
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static('public'));
-app.use(bodyParser.json());
-let proposal = {
-  text: '',
-  politicalp: '',
-  year: '',
-  candidate: '',
-};
-let resp = {
-  error: false,
-  codigo: 200,
-  mensaje: '',
-};
-// app.get('/', function (req, res) {
-//   resp = {
-//     error: true,
-//     codigo: 200,
-//     mensaje: 'Start point',
-//   };
-//   res.send(resp);
-// });
-app
-  .route('/proposal')
-  .get(function (req, res) {
-    resp = {
-      error: false,
-      codigo: 200,
-      mensaje: '',
-    };
-    if (
-      proposal.text === '' ||
-      proposal.politicalp === '' ||
-      proposal.year === '' ||
-      proposal.candidate === ''
-    ) {
-      resp = {
-        error: true,
-        codigo: 501,
-        mensaje: 'Proposal received',
-      };
-    } else {
-      resp = {
-        error: false,
-        codigo: 200,
-        mensaje: 'Proposal response',
-        resp: proposal,
-      };
-    }
-    res.send(resp);
-  })
-  .post(function (req, res) {
-    console.log(req.body);
-    if (
-      !req.body.text ||
-      !req.body.politicalp ||
-      !req.body.year ||
-      !req.body.candidate
-    ) {
-      resp = {
-        error: true,
-        codigo: 502,
-        mensaje: 'All the data is required',
-      };
-    } else {
-      if (
-        proposal.text !== '' ||
-        proposal.politicalp !== '' ||
-        proposal.year !== '' ||
-        proposal.candidate !== ''
-      ) {
-        resp = {
-          error: true,
-          codigo: 503,
-          mensaje: 'Proposal previously received',
-        };
-      } else {
-        proposal = {
-          text: req.body.text,
-          politicalp: req.body.politicalp,
-          year: req.body.year,
-          candidate: req.body.candidate,
-        };
-        resp = {
-          error: false,
-          codigo: 200,
-          mensaje: 'Proposal created',
-          respuesta: proposal,
-        };
-      }
-    }
-
-    res.send(resp);
-  })
-  .put(function (req, res) {
-    if (
-      !req.body.text ||
-      !req.body.politicalp ||
-      !req.body.year ||
-      !req.body.candidate
-    ) {
-      resp = {
-        error: true,
-        codigo: 502,
-        mensaje: 'All the data is required',
-      };
-    } else {
-      if (
-        proposal.text !== '' ||
-        proposal.politicalp !== '' ||
-        proposal.year !== '' ||
-        proposal.candidate !== ''
-      ) {
-        resp = {
-          error: true,
-          codigo: 501,
-          mensaje: 'Proposal not submitted',
-        };
-      } else {
-        proposal = {
-          text: req.body.text,
-          politicalp: req.body.politicalp,
-          year: req.body.year,
-          candidate: req.body.candidate,
-        };
-        resp = {
-          error: false,
-          codigo: 200,
-          mensaje: 'Proposal updated',
-          respuesta: proposal,
-        };
-      }
-    }
-
-    res.send(resp);
-  })
-  .delete(function (req, res) {
-    if (
-      proposal.text !== '' ||
-      proposal.politicalp !== '' ||
-      proposal.year !== '' ||
-      proposal.candidate !== ''
-    ) {
-      resp = {
-        error: true,
-        codigo: 501,
-        mensaje: 'Proposal not submitted',
-      };
-    } else {
-      resp = {
-        error: false,
-        codigo: 200,
-        mensaje: 'Proposal deleted',
-      };
-      proposal = {
-        text: req.body.text,
-        politicalp: req.body.politicalp,
-        year: req.body.year,
-        candidate: req.body.candidate,
-      };
-    }
-    res.send(resp);
-  });
-app.use(function (req, res, next) {
-  resp = {
-    error: true,
-    codigo: 404,
-    mensaje: 'URL not found',
-  };
-  res.status(404).send(resp);
-});
 app.listen(3000, () => {
   console.log('Server starting at port 3000');
 });
+app.use(express.static('public'));
+app.use(express.json({ limit: '1mb' }));
 
+const database = new Datastore('database.db');
+database.loadDatabase();
+
+//const bodyParser = require('body-parser');
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.json());
+
+
+app.get('/proposals', (request, response) => {
+  database.find({}, (err, data) => {
+    if (err) {
+      response.end();
+      return;
+    }
+    response.json(data);
+  });
+});
+
+app.post('/proposals', (request, response) => {
+  const data = request.body;
+  const timestamp = Date.now();
+  data.timestamp = timestamp;
+  database.insert(data);
+  response.json(data);
+  console.log("proposal received");
+  console.log(data);
+});
+
+
+// 
 // app.get('/search:/text',searchText);
 //
 // function searchText(request,response){
-//   var =request.params.text;
+//   var = request.params.proposal;
 //   var =resp;
 //   if(text[text]){
 //     status:"found";
 //   }
-// }
+//   }
